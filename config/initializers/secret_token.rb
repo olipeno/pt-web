bf595a8580e97766fd4ef653e755d54390e510f9
Prachi Sakharkar <olipeno@gmail.com>
2012-06-19 22:18:01 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PtWeb::Application.config.secret_token = '920898eaa119515a85e212e20b68ba68973758fda0ca94c2fa887ca0444f28495515394b9fe050bba4f71c2ee6905bcd9aef2b3a8a102871fcc09ed5ba5596cd'
